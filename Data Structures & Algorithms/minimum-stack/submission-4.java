fa81e8d4041a0c1f01f72b82d41d18c11df64ca6
Nurlan <44273779+ymetov@users.noreply.github.com>
2026-07-03 14:24:34 +0500

    Add: minimum-stack - submission-4

---
+++ Data Structures & Algorithms/minimum-stack/submission-4.java
@@ -0,0 +1,30 @@
+class MinStack {
+    Stack<Integer> stack;
+    Stack<Integer> minStack;
+
+    public MinStack() {
+        stack = new Stack<>();
+        minStack = new Stack<>();
+    }
+    
+    public void push(int val) {
+        stack.push(val);
+        if (minStack.isEmpty() || minStack.peek() >= val) {
+            minStack.push(val);
+        }
+    }
+    
+    public void pop() {
+        if (stack.isEmpty()) return;
+        int val = stack.pop();
+        if (minStack.peek() == val) minStack.pop();
+    }
+    
+    public int top() {
+        return stack.peek();
+    }
+    
+    public int getMin() {
+        return minStack.peek();
+    }
+}
